3c07297e6193b12d2642c39aa9219b8826de6278
rjmoreno2022 <rjmoreno2022@gmail.com>
2023-03-21 01:19:55 +0000

    Project Finished

--- src/app.js
+++ src/app.js
@@ -1,11 +1,62 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+// /* eslint-disable */
+// import "bootstrap";
+// import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+// import "./assets/img/rigo-baby.jpg";
+// import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+// window.onload = function() {
+//   //write your code here
+//   console.log("Hello Rigo from the console!");
+//   let pronoun = ['the','our'];
+//   let adj = ['great', 'big' ];
+//   let noun = ['jogger','racoon'];
+//   let extensions = ['.com','.net','.org', '.io','.us'];
+
+// };
+
+let pronoun = ["the", "our"];
+let adj = ["great", "big", "last"];
+let noun = ["jogger", "racoon", "OfUs"];
+let extensions = [".com", ".er", ".us"];
+let ListToPrint;
+
+function DomainList() {
+  let vDomList = [];
+  let vDomain = "";
+  pronoun.forEach((itemPronoun) => {
+    adj.forEach((itemAdj) => {
+      noun.forEach((itemNoun) => {
+        extensions.forEach((itemExt) => {
+          vDomain = itemPronoun + itemAdj + itemNoun + itemExt;
+          vDomList.push(vDomain);
+
+          if (
+            itemNoun.slice(itemExt.slice(1).length * -1).toLowerCase() ==
+            itemExt.slice(1).toLowerCase()
+          ) {
+            //#region Primer Intento usando Replace
+            // let NounWithoutExtension = itemNoun
+            //   .toLowerCase()
+            //   .replace(
+            //     itemNoun.slice(itemExt.slice(1).length * -1).toLowerCase(),
+            //     ""
+            //   );
+            // console.log(NounWithoutExtension);
+            //#endregion
+            let NounWithoutExtension = itemNoun.slice(
+              0,
+              itemNoun.length - itemExt.slice(1).length
+            );
+            vDomain = itemPronoun + itemAdj + NounWithoutExtension + itemExt;
+          }
+          vDomList.push(vDomain);
+        });
+      });
+    });
+  });
+  return vDomList;
+}
+
+ListToPrint = DomainList();
+console.log(ListToPrint);
